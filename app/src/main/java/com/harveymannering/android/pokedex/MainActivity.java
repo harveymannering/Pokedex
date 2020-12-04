e4acec6e0521c1fdbcb9229e3f3d719a686572c4
harveymannering <60523103+harveymannering@users.noreply.github.com>
2020-12-04 16:55:55 +0000

    Inital Project Commit

---
+++ app/src/main/java/com/harveymannering/android/pokedex/MainActivity.java
@@ -0,0 +1,25 @@
+package com.harveymannering.android.pokedex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
